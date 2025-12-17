29ac0a09c6c336aef88a54dc64c030810930ccc6
merna <mernaelia1234@gmail.com>
2025-12-17 02:06:48 +0200

    inventory and sales changes

--- app_one/__manifest__.py
+++ app_one/__manifest__.py
@@ -6,10 +6,10 @@
 
 'data': [
     'security/ir.model.access.csv',
-    'data/dashboard_data.xml',
+    'data/dashboard_sales.xml',
 "data/dashboard_inventory.xml",
     'views/app_one_view.xml',
-    'views/dashboard_view.xml',
+    'views/sales_view.xml',
     "views/inventory_view.xml",
 
 ],
