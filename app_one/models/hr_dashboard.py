ee30b45aa33070e2bf169256ff3a9199239aa7c4
Salma Waheed <SalmaWaheed92004@gmail.com>
2026-02-11 03:55:34 +0200

    add emp_turnover kpi and add emp_attendance kpi

--- app_one/models/hr_dashboard.py
+++ app_one/models/hr_dashboard.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import datetime,timedelta,date
 class HR_Dashboard(models.Model):
     _name = 'wb.hr.dashboard'
     _description = 'HR KPI Dashboard'
@@ -7,23 +8,67 @@ class HR_Dashboard(models.Model):
     workload_hours = fields.Float(string="total workload hours")
     tasks_complete = fields.Integer(string=" tasks_complete")
     production_level_kpi = fields.Float(string="productivity")
+    days = fields.Integer(string=' Attendance Days')
+    total_work_days = fields.Integer(string='Total Work Days')
+    attendance = fields.Float(string=' Employee Attendance ')
+    emp_turnover=fields.Float(string=' Employee TurnOver ')
+
     datajson = fields.Text()
     @api.model
-    def compute_kpis(self):
+    def compute_kpis(self,period=7):
+        period = int(period)
+        total_attendance = 0
+        total_work = 0
+        if (period == 0):
+            period = 7
+        datefilter = datetime.now()-timedelta(days=period)
+        datefilter_date = datefilter.date()
+        end_date = date.today()
+        start_date = end_date - timedelta(days=period)
         all_employees = self.env['hr.employee'].search([])
-        done_tasks = self.env['project.task'].search([('state', 'in', ['1_done', '1_canceled'])])
-        tasks_not_completed = self.env['project.task'].search([('state', 'not in', ['1_done', '1_canceled'])])
+        done_tasks = self.env['project.task'].search([('state', 'in', ['1_done', '1_canceled']),("create_date",">=",datefilter)])
+        tasks_not_completed = self.env['project.task'].search([('state', 'not in', ['1_done', '1_canceled']),('create_date','>=',datefilter)])
         employee_count = len(all_employees)
-        # kpi number1
+        #2.emp.attendace
+        for e in all_employees:
+            attendances = self.env['hr.attendance'].search([
+                ('employee_id','=',e.id),
+                ('check_in','>=',datefilter),
+            ])
+            attendancesDays = len(attendances)
+            leaves = self.env['hr.leave'].search([
+                ('employee_id','=',e.id),
+                ('state','=','validate'),
+                ('request_date_to','>=',datefilter.date())
+            ])
+            leave_days = 0
+            for leave in leaves:
+                start_date = max(leave.request_date_from, datefilter_date)
+                end_date = min(leave.request_date_to, date.today())
+                if end_date >= start_date:
+                    leave_days =leave_days+(end_date - start_date).days + 1
+            total_attendance=total_attendance+len(attendances)
+            Workdays = period - leave_days
+            if (Workdays < 0):
+                Workdays = 0
+            total_work = total_work + Workdays
+            days = total_attendance
+            total_work_days = total_work
+        if total_work > 0:
+          attendance = round((total_attendance / total_work) * 100,2)
+        else:
+            attendance = 0
+
+        #1.tasks compleleted
         tasks_complete = len(done_tasks)
 
         workload_hours = sum(task.allocated_hours for task in tasks_not_completed)
-        # kpi number3
+        # 3. productivity
         if employee_count > 0:
             production_kpi = (tasks_complete / employee_count) * 100
         else:
             production_kpi = 0
-        # kpi number5
+        # 5.workload
         employee_hours = {}
         for task in tasks_not_completed:
             hours = task.allocated_hours
@@ -34,6 +79,14 @@ class HR_Dashboard(models.Model):
                 share = hours / len(users)
                 for user in users:
                     employee_hours[user.name] = employee_hours.get(user.name, 0) + share
+        emp_start_year = self.env['hr.employee'].search([('active', '=', True), ('create_date', '<=', start_date)])
+        emp_end_year = self.env['hr.employee'].search([('active', '=', True), ('create_date', '<=', end_date)])
+        emp_left = self.env['hr.employee'].search([('active','=',False),('write_date','>=',start_date),('write_date','<=',end_date)])
+        avg_employees= (len(emp_start_year) + len(emp_end_year)) / 2
+        if (avg_employees > 0):
+            emp_turnover= round((len(emp_left) / avg_employees) * 100,2)
+        else:
+            emp_turnover = 0
         return {
             'employee_count': employee_count,
             'workload_hours': workload_hours,
@@ -41,4 +94,8 @@ class HR_Dashboard(models.Model):
             'production_kpi': production_kpi,
             'chart_labels': list(employee_hours.keys()),
             'chart_data': [i for i in employee_hours.values()],
+            'days': days,
+            'attendance': attendance,
+            'total_work_days': total_work_days,
+            'emp_turnover': emp_turnover,
         }
--- app_one/static/src/js/dashboard.js
+++ app_one/static/src/js/dashboard.js
@@ -2,7 +2,7 @@
 import { registry } from "@web/core/registry";
 import { loadJS } from "@web/core/assets";
 import { useService } from "@web/core/utils/hooks";
-import { Component, onWillStart, onMounted, useState } from "@odoo/owl";
+const { Component, onWillStart, onMounted, useState } =owl;
 export class HrDashboard extends Component {
     setup() {
         this.orm = useService("orm");
@@ -11,29 +11,42 @@ export class HrDashboard extends Component {
             workload_hours: 0,
             tasks_complete: 0,
             production_kpi: 0,
+            period:7,
         });
         onWillStart(async () => {
             await loadJS("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.min.js");
-            try {
-                const data = await this.orm.call("wb.hr.dashboard", "compute_kpis", []);
-                if (data) {
-                    this.state.employee_count = data.employee_count;
-                    this.state.workload_hours = data.workload_hours;
-                    this.state.tasks_complete = data.tasks_complete;
-                    this.state.production_kpi = data.production_kpi;
+            await this.downloaddata()
 
-                    this.chartLabels = data.chart_labels;
-                    this.chartData = data.chart_data;
-                }
-            } catch (e) {
-                console.error("Error", e);
-            }
         });
 
         onMounted(() => {
             this.renderChart();
         });
     }
+   async downloaddata()
+            {
+                try {
+                    const data = await this.orm.call("wb.hr.dashboard", "compute_kpis", [this.state.period]);
+                    if (data) {
+                        this.state.employee_count = data.employee_count;
+                        this.state.workload_hours = data.workload_hours;
+                        this.state.tasks_complete = data.tasks_complete;
+                        this.state.production_kpi = data.production_kpi;
+                        this.state.attendance=data.attendance;
+                        this.state.emp_turnover=data.emp_turnover
+
+                        this.chartLabels = data.chart_labels;
+                        this.chartData = data.chart_data;
+                    }
+                } catch (e) {
+                    console.error("Error", e);
+                }
+            }
+      async onChangePeriod(op){
+            this.state.date=op.target.value;
+             await this.downloaddata();
+             await this.renderChart();
+        }
 
     renderChart() {
         const ctx = document.querySelector('.my_dashboard_chart');
