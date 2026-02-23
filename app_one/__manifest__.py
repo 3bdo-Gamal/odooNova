452cbb43e4450d44b64b6f586f9045100411dc50
merna <mernaelia1234@gmail.com>
2026-02-23 04:27:06 +0200

    sales_dashboard

--- app_one/__manifest__.py
+++ app_one/__manifest__.py
@@ -2,34 +2,36 @@
     'name': 'odooNova',
     'version': '1.0',
     'author': '3bdo',
-'depends': ['base', 'sale', 'board','hr','hr_attendance','project','hr_holidays','purchase','account','purchase_requisition'],
+    'depends': ['base', 'sale', 'board', 'hr', 'hr_attendance', 'project', 'hr_holidays', 'purchase', 'account',
+                'purchase_requisition'],
 
-'data': [
-    'security/ir.model.access.csv',
-    'data/dashboard_sales.xml',
-    "data/dashboard_inventory.xml",
-    'data/dashboard_hr.xml',
-    'views/app_one_view.xml',
-    'views/sales_view.xml',
-    "views/purchase_bills_view.xml",
-    "views/inventory_view.xml",
-    "views/hr_view.xml",
-    'views/PO.xml',
-],
- "assets":{
-"web.assets_backend":[
-         "app_one/static/src/js/bills_dashboard.js",
-         "app_one/static/src/xml/bills_dashboard.xml",
-         "app_one/static/src/js/dashboard.js",
-         "app_one/static/src/xml/dashboard.xml",
-         "app_one/static/src/xml/po_dashboard.xml",
-         "app_one/static/src/js/po_dashboard.js",
-         "app_one/static/src/js/inventory_dashboard.js",
-        "app_one/static/src/xml/inventory_dashboard.xml"
+    'data': [
+        'security/ir.model.access.csv',
+        "data/dashboard_inventory.xml",
+        'data/dashboard_hr.xml',
+        'views/app_one_view.xml',
+        'views/sales_view.xml',
+        "views/purchase_bills_view.xml",
+        "views/inventory_view.xml",
+        "views/hr_view.xml",
+        'views/PO.xml',
+    ],
 
-]
- },
+    "assets": {
+        "web.assets_backend": [
 
+            "app_one/static/src/js/bills_dashboard.js",
+            "app_one/static/src/xml/bills_dashboard.xml",
+            "app_one/static/src/js/dashboard.js",
+            "app_one/static/src/xml/dashboard.xml",
+            "app_one/static/src/xml/po_dashboard.xml",
+            "app_one/static/src/js/po_dashboard.js",
+            "app_one/static/src/js/inventory_dashboard.js",
+            "app_one/static/src/xml/inventory_dashboard.xml",
+            "app_one/static/src/js/Sales_dashboard.js",
+            "app_one/static/src/xml/sales_dashboard.xml"
+        ]
+    },
 
     'application': True,
 }
